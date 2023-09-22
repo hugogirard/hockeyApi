a8344f7e423c807ad60232e082dec043aa6d9e2b
Hugo Girard <hugirard@microsoft.com>
2023-09-22 17:02:53 -0400

    complete api

--- server.js
+++ server.js
@@ -1,42 +1,18 @@
 const express = require('express');
 const { canadianTeams, eastCoastUsaTeams } = require('./model/team.js');
+const simulatorService = require('./service/simulator.js')();
 const app = express();
 
 // In-memory database to store game results
 const gameResults = [];
 
 app.get('/api/game-results/:date', (req, res) => {
-    const date = new Date(req.params.date);
-  
-    const resultsForDate = gameResults.filter((result) => {
-      return (
-        result.date.toDateString() === date.toDateString() &&
-        (canadianTeams.includes(result.homeTeam) || eastCoastUsaTeams.includes(result.homeTeam))
-      );
-    });
-  
-    res.json(resultsForDate);
-});
 
-app.get('/api/team-results/:teamName', (req, res) => {
-  const teamName = req.params.teamName;
+  const requestedDate = req.params.date;
 
-  const teamResults = gameResults
-    .filter((result) =>
-      (result.homeTeam === teamName || result.awayTeam === teamName) &&
-      result.periods >= 3
-    )
-    .slice(-5)
-    .map((result) => {
-      return {
-        homeTeam: result.homeTeam,
-        awayTeam: result.awayTeam,
-        resultAfterThirdPeriod: `${result.homeScore} - ${result.awayScore}`,
-        date: result.date,
-      };
-    });
+  const gameResult = simulatorService.simulateGameResult(requestedDate,canadianTeams,eastCoastUsaTeams);
 
-  res.json(teamResults);
+  res.json(gameResult);
 });
 
 const port = process.env.PORT || 3000;
--- service/simulator.js
+++ service/simulator.js
@@ -2,24 +2,68 @@
 
 class simulator {
 
-    simulateGameResult(homeTeam, awayTeam) {
+    constructor() { 
+      this.gameResults = [];
+    }
 
-        const homeScore = Math.floor(Math.random() * 6);
-        const awayScore = Math.floor(Math.random() * 6);
-        const periods = Math.floor(Math.random() * 3) + 1; // 1 to 3 periods
-      
+    shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+
+    simulateGameResult(date, canadianTeams, eastCoastUsaTeams) {
+
+      // Check if games for this date have already been simulated
+      if (this.gameResults[date]) {
+        return this.gameResults[date];
+      }
+
+      const numGames = Math.min(10, Math.floor(Math.random() * 10) + 1); // Random number of games (1 to 10)
+      const maxGoals = 10;
+
+      // Shuffle the team arrays to ensure randomness
+      this.shuffleArray(canadianTeams);
+      this.shuffleArray(eastCoastUsaTeams);
+
+      const results = [];
+
+      for (let i = 0; i < numGames; i++) {
+        const homeTeam = canadianTeams[i % canadianTeams.length];
+        const awayTeam = eastCoastUsaTeams[i % eastCoastUsaTeams.length];
+
+        const periods = Math.min(3, Math.floor(Math.random() * 3) + 1); // Random number of periods (1 to 3)
+        const homeTeamScore = [];
+        const awayTeamScore = [];
+
+        for (let period = 1; period <= periods; period++) {
+          const homeGoals = Math.floor(Math.random() * (maxGoals + 1)); // Random goals for home team (0 to maxGoals)
+          const awayGoals = Math.floor(Math.random() * (maxGoals + 1)); // Random goals for away team (0 to maxGoals)
+
+          homeTeamScore.push(homeGoals);
+          awayTeamScore.push(awayGoals);
+        }
+
+        const finalHomeScore = homeTeamScore.reduce((acc, score) => acc + score, 0);
+        const finalAwayScore = awayTeamScore.reduce((acc, score) => acc + score, 0);
+
         const gameResult = {
           homeTeam,
           awayTeam,
-          homeScore,
-          awayScore,
-          periods,
-          date: new Date(),
+          homeTeamScore,
+          awayTeamScore,
+          finalHomeScore,
+          finalAwayScore,
         };
-      
-        gameResults.push(gameResult);
-      
-        return gameResult;
+
+        results.push(gameResult);
+      }
+
+      // Store the game results in the database
+      this.gameResults[date] = results;
+
+      return results;
     }
 }
 
